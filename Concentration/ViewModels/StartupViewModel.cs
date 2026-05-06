f022438c4d44a1310bcea2dbcac593f65bb5d325
Paul Johnson <paul.johnson@touchstar.co.uk>
2026-05-06 08:31:31 +0100

    add highscores to the vm

--- Concentration/ViewModels/StartupViewModel.cs
+++ Concentration/ViewModels/StartupViewModel.cs
@@ -1,15 +1,48 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using Concentration.Enums;
+using Concentration.Interfaces;
+using Concentration.Models;
 
 namespace Concentration.ViewModels
 {
     public partial class StartupViewModel : BaseViewModel
     {
+        IRepository? repository;
+
         [RelayCommand]
         async Task SetDifficulty(string button)
         {
-            DifficultLevel = button == "0" ? Difficulty.Easy : Difficulty.Hard;
+            switch (button)
+            {
+                case "0":
+                    DifficultLevel = Difficulty.Easy;
+                    break;
+                case "1":
+                    DifficultLevel = Difficulty.Medium;
+                    break;
+                case "2":
+                    DifficultLevel = Difficulty.Hard;
+                    break;
+
+            }
             await Shell.Current.GoToAsync("//NewGame");
+        }
+
+        public StartupViewModel()
+        {
+            repository = App.Service.GetService<IRepository>();
+        }
+
+        public async Task Init()
+        {
+            var hiScores = await repository.GetList<HighScoreModel>(10);
+            if (hiScores.Count <= 10) // table is empty
+            {
+                for (var i = hiScores.Count; i < 10; i++)
+                {
+                    await repository.SaveData(new HighScoreModel { Entered = DateTime.Now, Name = "The chump", Score = 1, Difficulty = (int)Difficulty.Easy });
+                }
+            }
         }
     }
 }
