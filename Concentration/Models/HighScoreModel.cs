41450ec6459b1a57a3ab95b97d9d822ed3d83803
Paul Johnson <paul.johnson@touchstar.co.uk>
2026-05-06 08:30:45 +0100

    Add difficulty property

---
+++ Concentration/Models/HighScoreModel.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace Concentration.Models;
+
+public class HighScoreModel
+{
+    [PrimaryKey]
+    public Guid Id { get; set; }
+    [MaxLength(24)]
+    public string Name { get; set; } = "";
+    public int Score { get; set; }
+    public DateTime Entered { get; set; } = DateTime.Now;
+    public int Difficulty { get; set; }
+}
