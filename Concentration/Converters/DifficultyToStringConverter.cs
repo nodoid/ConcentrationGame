37225044bf620d48ad256e127a6b97d48f321044
Paul Johnson <paul.johnson@touchstar.co.uk>
2026-05-06 08:29:28 +0100

    Add converter

---
+++ Concentration/Converters/DifficultyToStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using Concentration.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Concentration.Converters
+{
+    public class DifficultyToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var prop = (Difficulty)parameter;
+
+            return prop == Difficulty.Hard ? "Hard" : (prop == Difficulty.Easy ? "Easy" : "Medium");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) => string.Empty;
+    }
+}
