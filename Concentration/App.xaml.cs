5dd16c80204636d8b52545da728f7fa55a60fdb1
Paul Johnson <paul.johnson@touchstar.co.uk>
2026-05-06 08:34:19 +0100

    modify to new style of window start add code for database

--- Concentration/App.xaml.cs
+++ Concentration/App.xaml.cs
@@ -1,12 +1,46 @@
-﻿namespace Concentration
+﻿using Concentration.Database;
+using Concentration.Interfaces;
+using SQLite;
+
+namespace Concentration
 {
     public partial class App : Application
     {
+        public static IServiceProvider Service { get; private set; }
+        public static App Self { get; private set; }
+        public static SQLiteAsyncConnection SQLConnection { get; set; }
+
         public App()
         {
-            InitializeComponent();
+            App.Self = this;
 
-            MainPage = new AppShell();
+            Service = Startup.Init();
+            InitializeComponent();
+            
+            SetupSQLiteConnection();
+
+            // create SQLite repositiory class
+            new SqLiteRepository();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState) => new Window(new AppShell());
+
+        void SetupSQLiteConnection()
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            path = Path.Combine(path, "conchiscore.db");
+
+            try
+            {
+                App.SQLConnection = new SQLiteAsyncConnection(path, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                Console.WriteLine(
+                    $"Connection could  not be made : ex.Message = {ex.Message} - inner = {ex.InnerException?.Message}");
+#endif
+            }
         }
     }
 }
