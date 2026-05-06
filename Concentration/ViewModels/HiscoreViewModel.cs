012f0dddb5811afd39f0b78082ee5e3805b50a8d
Paul Johnson <paul.johnson@touchstar.co.uk>
2026-05-06 08:32:27 +0100

    new vm

---
+++ Concentration/ViewModels/HiscoreViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Concentration.Helpers;
+using Concentration.Interfaces;
+using Concentration.Models;
+using System.Collections.ObjectModel;
+
+namespace Concentration.ViewModels
+{
+    public partial class HiscoreViewModel : BaseViewModel
+    {
+        IRepository? repository;
+
+        [ObservableProperty]
+        ObservableCollection<HighScoreModel> highScores;
+
+        [RelayCommand]
+        public async Task CloseHighscores() => await Mopups.Services.MopupService.Instance.PopAsync();
+
+        public HiscoreViewModel() : base()
+        {
+            repository = Startup.ServiceProvider.GetService<IRepository>();
+        }
+
+        public HiscoreViewModel(IRepository repo) :base()
+        {
+            repository = repo;
+        }
+
+        public async Task Init()
+        {
+            HighScores = (await repository.GetList<HighScoreModel>(10)).ToObservable();
+        }
+    }
+}
