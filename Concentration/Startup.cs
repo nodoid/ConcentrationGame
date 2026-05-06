eb6dfd82e649fbe34d72dfc5b5e2e053e1655154
Paul Johnson <paul.johnson@touchstar.co.uk>
2026-05-06 08:34:42 +0100

    add viewmodels

---
+++ Concentration/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Concentration.Helpers;
+
+namespace Concentration
+{
+    public class Startup 
+	{
+		public static IServiceProvider ServiceProvider { get; set; }
+
+		public static IServiceProvider Init()
+		{
+			var provider = new ServiceCollection().
+				ConfigureServices().
+				ConfigureViewModels().
+				BuildServiceProvider();
+
+			ServiceProvider = provider;
+
+			return provider;
+		}
+	}
+}
