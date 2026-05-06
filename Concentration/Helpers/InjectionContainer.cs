9ea502d2efac4d951e4212ea0ada37619f662440
Paul Johnson <paul.johnson@touchstar.co.uk>
2026-05-06 08:30:12 +0100

    remove unused code

---
+++ Concentration/Helpers/InjectionContainer.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Concentration.Database;
+using Concentration.Interfaces;
+using Concentration.ViewModels;
+
+namespace Concentration.Helpers
+{
+    public static class  InjectionContainer
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services)
+        {
+            var i = new ServiceCollection();
+
+            i.AddSingleton<IRepository, SqLiteRepository>().
+            AddSingleton<IMessenger>(WeakReferenceMessenger.Default);
+
+            services = i;
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureViewModels(this IServiceCollection services)
+        {
+            services.AddTransient<BaseViewModel>();
+            services.AddScoped<GameViewModel>();
+            services.AddScoped<StartupViewModel>();
+
+            return services;
+        }
+
+    }
+}
